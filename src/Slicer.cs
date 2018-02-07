35a3ba1869187c58ba7b3b628a04b0a87875e34e
Jaerdon <jordandavidwhiting@gmail.com>
2018-02-06 21:43:26 -0800

    Fix formatting

--- src/Slicer.cs
+++ src/Slicer.cs
@@ -1,12 +1,19 @@
-﻿using Slicer.formats;
+﻿using Slicer.models;
 
 namespace Slicer {
 
     internal class Slicer {
+        
         public static void Main(string[] args) {
-            const string path = "Wheel v4.stl";
-            var stl = new StlFile(path);
+            var slicer = new Slicer();
+            slicer.Run();
         }
+
+        private void Run() {
+            const string path = "Wheel v4.stl";
+            var model = Model3D.CreateFromStl(path);
+        }
+        
     }
 
 }
