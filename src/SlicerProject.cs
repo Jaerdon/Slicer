2eb88ab8a46c0c6707532f662884200dbf844992
Jaerdon <jordandavidwhiting@gmail.com>
2018-10-13 14:12:53 -0700

    Added infill percent, export format, and layer height command line arguments.

--- src/SlicerProject.cs
+++ src/SlicerProject.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Net;
 using Slicer.Formats;
 using Slicer.models;
 
@@ -27,6 +26,11 @@ namespace Slicer
                 float rotateY = 0.0f;
                 float rotateZ = 0.0f;
 
+                float layerHeight = 0.2f;
+                float infill = 0.15f;
+
+                ExportFormat format = ExportFormat.GCode;
+
                 //Parse Arguments
                 for (int i = 1; i < args.Length; i++)
                 {
@@ -43,12 +47,25 @@ namespace Slicer
                             translateY = float.Parse(args[i + 1]);
                         else if (args[i].Equals("-tZ") || args[i].Equals("--translateZ"))
                             translateZ = float.Parse(args[i + 1]);
-                        if (args[i].Equals("-rX") || args[i].Equals("--rotateX"))
+                        else if (args[i].Equals("-rX") || args[i].Equals("--rotateX"))
                             rotateX = float.Parse(args[i + 1]);
                         else if (args[i].Equals("-rY") || args[i].Equals("--rotateY"))
                             rotateY = float.Parse(args[i + 1]);
                         else if (args[i].Equals("-rZ") || args[i].Equals("--rotateZ"))
                             rotateZ = float.Parse(args[i + 1]);
+                        else if (args[i].Equals("-l") || args[i].Equals("--layerHeight"))
+                            layerHeight = float.Parse(args[i + 1]);
+                        else if (args[i].Equals("-i") || args[i].Equals("--infill"))
+                            infill = float.Parse(args[i + 1]);
+                        else if (args[i].Equals("-e") || args[i].Equals("--export"))
+                        {
+                            if (args[i + 1].ToLower().Equals("gcode")) 
+                                format = ExportFormat.GCode;
+                            else if (args[i + 1].ToLower().Equals("svg") || args[i + 1].ToLower().Equals("vector"))
+                                format = ExportFormat.SVG;
+                            else
+                                DisplayErrorMessage();
+                        }
                     }
                 }
                 
@@ -69,7 +86,7 @@ namespace Slicer
                     model.Translate(translateX, translateY, translateZ);
                     model.Rotate(rotateX, rotateY, rotateZ);
                 }
-                slicer.SliceByLayer(model, 0.2f, 0.15f, ExportFormat.GCode);
+                slicer.SliceByLayer(model, layerHeight, infill, format);
                 Console.WriteLine("Done slicing " + Path.GetFileName(file));
             }
 
@@ -80,13 +97,16 @@ namespace Slicer
         {
             Console.WriteLine("Usage: slice [OPTION]... [FILE]...\n" +
                               "Slices CAD FILE into GCODE FILE.\n\n" +
-                              "-tX n, --translateX n      translate x component by some amount n\n" +
-                              "-tY n, --translateY n      translate y component by some amount n\n" +
-                              "-tZ n, --translateZ n      translate z component by some amount n\n" +
-                              "-rX n, --rotateX n         rotate model around x axis by some amount n\n" +
-                              "-rY n, --rotateY n         rotate model around y axis by some amount n\n" +
-                              "-rZ n, --rotateZ n         rotate model around z axis by some amount n\n" +
-                              "\n-s, --swap               swap rotation and transformation order, default is transform first\n" +
+                              "-tX [n], --translateX [n]      translate x component by some amount n\n" +
+                              "-tY [n], --translateY [n]      translate y component by some amount n\n" +
+                              "-tZ [n], --translateZ [n]      translate z component by some amount n\n" +
+                              "-rX [n], --rotateX [n]         rotate model around x axis by some amount n\n" +
+                              "-rY [n], --rotateY [n]         rotate model around y axis by some amount n\n" +
+                              "-rZ [n], --rotateZ [n]         rotate model around z axis by some amount n\n" +
+                            "\n-s, --swap                       swap rotation and transformation order, default is transform first\n" +
+                              "-l [n], --layerHeight [n]        set layerheight to n, default is 0.2mm\n" +
+                              "-i [n], --infill [n]             set infill percentage to n, default is 15% (0.15)\n" +
+                              "-f [format], --format [format]   change export format type (ex. GCode, SVG)\n" +
                               "\n-h, --help display this help\n");
         }
 
